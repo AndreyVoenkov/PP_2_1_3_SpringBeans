9999ba4972e0702b1033e8cd873bbe10050ea3fe
Andrey198o <voenkov.java.dev@gmail.com>
2024-04-17 21:27:04 +0300

    Finish task

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -14,15 +14,15 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
+    public Timer getTimer() {
+        return timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
-    }
-
-    public Timer getTimer() {
-        return timer;
     }
 }
